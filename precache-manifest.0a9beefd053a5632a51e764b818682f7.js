221397afee0d0a847f69929ddec173db9be91572
sybilla11 <sybilla11@users.noreply.github.com>
2020-10-24 21:36:02 -0700

    Updates

---
+++ precache-manifest.0a9beefd053a5632a51e764b818682f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bd79a8e71d254a53a72604e5db3007b",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "a48c64e2755893819eee",
+    "url": "/monster-rolodex/static/css/main.8d02098c.chunk.css"
+  },
+  {
+    "revision": "2d19631dba197ad7891e",
+    "url": "/monster-rolodex/static/js/2.e1627e68.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monster-rolodex/static/js/2.e1627e68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a48c64e2755893819eee",
+    "url": "/monster-rolodex/static/js/main.10b188fd.chunk.js"
+  },
+  {
+    "revision": "212570b3a5f291f8b8f3",
+    "url": "/monster-rolodex/static/js/runtime-main.4a41f6c3.js"
+  }
+]);
